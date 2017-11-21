0629263658029782683cee818244f859537849ec
Jani Kaarela <jani.kaarela@gmail.com>
2017-11-21 09:46:29 +0100

    Separate move() method, movement "cool down".

--- src/main/game.js
+++ src/main/game.js
@@ -7,11 +7,12 @@ const maps    = require('./map');
 window.onload = function() {
     const tileSize = 50;
     const tileCount = 11;
+    const movementCooldownTime = Phaser.Timer.QUARTER;
     const windowSize = tileSize * tileCount;
     const grassTileRef = "grass";
     const bunnySpriteRef = "bunny";
     const game = new Phaser.Game(windowSize, windowSize, Phaser.CANVAS, "game", { preload: preload, create: create, update: update, render: render });
-    const state = { game: game };
+    const state = { game: game, movementCooldown: false };
 
     function preload() {
         game.load.image(grassTileRef, "images/grass_t.png");
@@ -26,22 +27,37 @@ window.onload = function() {
         const tiles = {
             grass: grassTileRef
         };
-        state.game.time.slowMotion = 4.0;
         state.map = maps.createMap(game, settings, tiles);
         state.player = game.add.sprite(0, 0, bunnySpriteRef);
     }
 
+    function setCooldown() {
+        state.movementCooldown = true;
+        game.time.events.add(movementCooldownTime, unsetCooldown, this);
+    }
+
+    function unsetCooldown() {
+        state.movementCooldown = false;
+    }
+
+    function move(callback) {
+        if (!state.movementCooldown) {
+            callback();
+            setCooldown();
+        }
+    }
+
     function update() {
         if (state.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            state.player.y -= tileSize;
+            move(() => { state.player.y -= tileSize });
         } else if (state.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            state.player.y += tileSize;
+            move(() => { state.player.y += tileSize });
         }
 
         if (state.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            state.player.x -= tileSize;
+            move(() => { state.player.x -= tileSize });
         } else if (state.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            state.player.x += tileSize;
+            move(() => { state.player.x += tileSize });
         }
     }
 
