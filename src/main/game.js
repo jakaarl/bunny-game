74dbabf14804c48fa8c62a68e244b0abdc5b24d9
Niko Viik <Niko.Viik@Wunderdog.fi>
2017-11-24 13:13:36 +0100

    Initial bunny coder commit

--- src/main/game.js
+++ src/main/game.js
@@ -4,36 +4,62 @@ window.p2     = require('phaser-ce/build/custom/p2');
 window.Phaser = require('phaser-ce/build/custom/phaser-split');
 const maps    = require('./map');
 
+const TILE_SIZE = 32;
+const TILE_COUNT = 10;
+const WINDOW_SIZE = TILE_SIZE * TILE_COUNT;
+const MOVEMENT_COOLDOWN_TIME = Phaser.Timer.QUARTER;
+
+const GRASS_TILE_REF = "grass";
+const BUNNY_SPRITE_REF = "bunny";
+const TILES_REF = "ground_1x1";
+const WALLS_REF = "walls_1x2"
+
 window.onload = function () {
-    const tileSize = 50;
-    const tileCount = 11;
-    const movementCooldownTime = Phaser.Timer.QUARTER;
-    const windowSize = tileSize * tileCount;
-    const grassTileRef = "grass";
-    const bunnySpriteRef = "bunny";
-    const game = new Phaser.Game(windowSize, windowSize, Phaser.CANVAS, "game", { preload: preload, create: create, update: update, render: render });
+
+    const game = new Phaser.Game(WINDOW_SIZE, WINDOW_SIZE, Phaser.CANVAS, "game", {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    });
+  
     const state = { movementCooldown: false };
 
     function preload() {
-        game.load.image(grassTileRef, "images/grass_t.png");
-        game.load.image(bunnySpriteRef, "images/bunny_s.png");
-    }
+        game.load.image(TILES_REF, "images/tiles.png")
+        // game.load.image(GRASS_TILE_REF, "images/grass_t.png");
+        // game.load.image(KANTO_TILE_REF, "images/kanto.png");
+        game.load.image(BUNNY_SPRITE_REF, "images/bunny_s.png");
+      }
 
     function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE)
+
         const settings = {
-            tileSize: tileSize,
-            tileCount: tileCount
+            tileSize: TILE_SIZE,
+            tileCount: TILE_COUNT
         };
-        const tiles = {
-            grass: grassTileRef
-        };
-        state.map = maps.createMap(game, settings, tiles);
-        state.player = game.add.sprite(0, 0, bunnySpriteRef);
+        // const tiles = {
+        //     grass: GRASS_TILE_REF,
+        //     kanto: KANTO_TILE_REF
+        // };
+        // state.map = maps.createLevel1(game, settings, TILES_REF);
+//        state.map = maps.createMap(game, settings, tiles);
+
+
+        const createRes = maps.createLevel1(game, settings, TILES_REF);
+        state.map = createRes.tileMap
+        state.layer = createRes.layer
+
+        state.player = game.add.sprite(32, 32, BUNNY_SPRITE_REF);
+        game.physics.enable(state.player)
+        // state.player.body.setSize(100, 150,100, 50);
+        state.player.body.collideWorldBounds = true;
     }
 
     function setCooldown() {
         state.movementCooldown = true;
-        game.time.events.add(movementCooldownTime, unsetCooldown, this);
+        game.time.events.add(MOVEMENT_COOLDOWN_TIME, unsetCooldown, this);
     }
 
     function unsetCooldown() {
@@ -49,26 +75,43 @@ window.onload = function () {
     }
 
     function wrapAroundAtBounds(coord) {
-        if (coord < 0) return windowSize + coord;
-        if (coord >= windowSize) return coord - windowSize;
+        if (coord < 0) return WINDOW_SIZE + coord;
+        if (coord >= WINDOW_SIZE) return coord - WINDOW_SIZE;
         return coord;
     }
 
     function update() {
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            moveTo(state.player.x, state.player.y - tileSize);
+            moveTo(state.player.x, state.player.y - TILE_SIZE);
         } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            moveTo(state.player.x, state.player.y + tileSize);
+            moveTo(state.player.x, state.player.y + TILE_SIZE);
         }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            moveTo(state.player.x - tileSize, state.player.y);
+            moveTo(state.player.x - TILE_SIZE, state.player.y);
         } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            moveTo(state.player.x + tileSize, state.player.y);
+            moveTo(state.player.x + TILE_SIZE, state.player.y);
         }
+
+        game.physics.arcade.collide(state.player, state.layer);
     }
 
     function render() {
-        
+
     }
+
+    window.vasen = function() {
+        moveTo(state.player.x - TILE_SIZE, state.player.y);
+    }
+
+    window.execute = function() {
+        eval(document.getElementById("code").value);
+        // var square = document.getElementById("square");
+        // if (leveys) {
+        //   square.style.width = leveys + "px";
+        // }
+        // if (korkeus) {
+        //   square.style.height = korkeus + "px";
+        // }
+      }
 }
--- src/main/map.js
+++ src/main/map.js
@@ -12,3 +12,50 @@ module.exports.createMap = function(game, settings, tiles) {
     }
     return tileMap;
 }
+
+module.exports.createLevel1 = function(game, settings, tiles) {
+  const tileMap = game.add.tilemap();
+  console.log('tiles', tiles)
+  tileMap.addTilesetImage(tiles);
+  tileMap.setCollisionBetween(1, 5); 
+  tileMap.setCollision(1);
+
+  const layer = tileMap.create("layerGround", settings.tileCount, settings.tileCount, settings.tileSize, settings.tileSize);
+  layer.resizeWorld();
+
+  // const collisionLayer = map.tileMap('Collision')
+  // collisionLayer.visible = false;
+  // tileMap.setCollisionByExclusion([], true, this.collisionLayer);
+  // collisionLayer.resizeWorld();
+
+  // 0 = grass (33,107)
+  // 1 = kanto
+  const mapData = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ];
+
+  for (let y = 0; y < settings.tileCount; ++y) {
+    for (let x = 0; x < settings.tileCount; ++x) {
+      if (mapData[y][x] === 0) {
+        tileMap.putTile(0, x, y, layer);
+      }
+
+      if (mapData[y][x] === 1) {
+        tileMap.putTile(1, x, y, layer);
+      }
+    }
+  }
+  return {
+    tileMap,
+    layer
+  }
+}
