8bff001e5169ac5302217894b8a093523edcaad8
Niko Viik <Niko.Viik@Wunderdog.fi>
2017-11-24 14:05:57 +0100

    Update game

--- src/main/game.js
+++ src/main/game.js
@@ -7,164 +7,115 @@ const maps    = require('./map');
 const TILE_SIZE = 32;
 const TILE_COUNT = 10;
 const WINDOW_SIZE = TILE_SIZE * TILE_COUNT;
-const MOVEMENT_COOLDOWN_TIME = Phaser.Timer.QUARTER;
-
-const GRASS_TILE_REF = "grass";
+const MOVEMENT_COOLDOWN_TIME_MS = 500;
 const BUNNY_SPRITE_REF = "bunny";
-const TILES_REF = "ground_1x1";
-const WALLS_REF = "walls_1x2"
+const TILES_REF = "tiles";
 
 window.onload = function () {
+  const game = new Phaser.Game(WINDOW_SIZE, WINDOW_SIZE, Phaser.CANVAS, "game", {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+  });
 
-    const game = new Phaser.Game(WINDOW_SIZE, WINDOW_SIZE, Phaser.CANVAS, "game", {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    });
-  
-    const state = {
-      movementCooldown: false,
-      playerTilePos: {
-        x: 1,
-        y: 1
-      }
+  const state = {
+    movementCooldown: false,
+    playerTilePos: {
+      x: 1,
+      y: 1
+    },
+    currentLevel: 1
+  }
+
+  function preload() {
+    game.load.image(TILES_REF, "images/tiles.png")
+    game.load.image(BUNNY_SPRITE_REF, "images/bunny_s.png");
+  }
+
+  function create() {
+    const settings = {
+      tileSize: TILE_SIZE,
+      tileCount: TILE_COUNT
     };
 
-    function preload() {
-        game.load.image(TILES_REF, "images/tiles.png")
-        // game.load.image(GRASS_TILE_REF, "images/grass_t.png");
-        // game.load.image(KANTO_TILE_REF, "images/kanto.png");
-        game.load.image(BUNNY_SPRITE_REF, "images/bunny_s.png");
-      }
+    const createRes = maps.loadLevel(game, settings, TILES_REF, state.currentLevel);
+    state.map = createRes.tileMap
+    state.layer = createRes.layer
+    state.mapData = createRes.mapData
 
-    function create() {
-        // game.physics.startSystem(Phaser.Physics.ARCADE)
+    state.player = game.add.sprite(32, 32, BUNNY_SPRITE_REF);
+  }
 
-        const settings = {
-            tileSize: TILE_SIZE,
-            tileCount: TILE_COUNT
-        };
-        // const tiles = {
-        //     grass: GRASS_TILE_REF,
-        //     kanto: KANTO_TILE_REF
-        // };
-        // state.map = maps.createLevel1(game, settings, TILES_REF);
-//        state.map = maps.createMap(game, settings, tiles);
+  function checkCollision(tileX, tileY) {
+    return state.mapData[tileY][tileX];
+  }
 
+  function movePlayer(x, y) {
+    const newX = state.playerTilePos.x + x;
+    const newY = state.playerTilePos.y + y;
+    const collisionType = checkCollision(newX, newY);
 
-        const createRes = maps.createLevel1(game, settings, TILES_REF);
-        state.map = createRes.tileMap
-        state.layer = createRes.layer
-        state.mapData = createRes.mapData
-
-        state.player = game.add.sprite(32, 32, BUNNY_SPRITE_REF);
-        // game.physics.enable(state.player)
-        // // state.player.body.setSize(100, 150,100, 50);
-        // state.player.body.collideWorldBounds = true;
+    if (collisionType === 0 || collisionType === 2) {
+      state.player.x = TILE_SIZE * newX;
+      state.player.y = TILE_SIZE * newY;
+      state.playerTilePos.x = newX;
+      state.playerTilePos.y = newY;
     }
 
-    function setCooldown() {
-        state.movementCooldown = true;
-        game.time.events.add(MOVEMENT_COOLDOWN_TIME, unsetCooldown, this);
+    if (collisionType === 2) {
+      console.log('VOITTO')
+    }
+  }
+
+  function update() {
+  }
+
+  function render() {
+  }
+
+  // PUBLIC API
+
+  window.vasen = function() {
+    movePlayer(-1, 0);
+  }
+
+  window.oikea = function() {
+    movePlayer(1, 0);
+  }
+
+  window.ylos = function() {
+    movePlayer(0, -1);
+  }
+
+  window.alas = function() {
+    movePlayer(0, 1);
+  }
+
+  window.execute = function() {
+    const text = document.getElementById("code").value;
+
+    if (text.length === 0) {
+      return;
     }
 
-    function unsetCooldown() {
-        state.movementCooldown = false;
-    }
+    const commands = text.split('\n');
+    console.log('commands', commands);
 
-    function moveTo(x, y) {
-      if (!state.movementCooldown) {
-        state.player.x = wrapAroundAtBounds(x);
-        state.player.y = wrapAroundAtBounds(y);
-        setCooldown();
-      }
-    }
+    let currentCommandIndex = 0;
 
-    function wrapAroundAtBounds(coord) {
-        if (coord < 0) return WINDOW_SIZE + coord;
-        if (coord >= WINDOW_SIZE) return coord - WINDOW_SIZE;
-        return coord;
-    }
+    function executeNextCommand() {
+      const command = commands[currentCommandIndex];
+      console.log('executing command', command);
+      eval(command);
 
-    function checkCollision(tileX, tileY) {
-      return state.mapData[tileY][tileX];
-    }
+      currentCommandIndex++;
 
-    function movePlayer(x, y) {
-      console.log('movePlayer', state.playerTilePos.x)
-      const newX = state.playerTilePos.x + x;
-      const newY = state.playerTilePos.y + y;
-      console.log('new pos', newX, newY)
-      const collisionType = checkCollision(newX, newY);
-      console.log('collisionType', collisionType)
-
-      if (collisionType === 0) {
-        state.player.x = TILE_SIZE * newX;
-        state.player.y = TILE_SIZE * newY;
-        state.playerTilePos.x = newX;
-        state.playerTilePos.y = newY;
-      }
-
-      if (collisionType === 2) {
-        console.log('VOITTO')
+      if (currentCommandIndex < commands.length) {
+        setTimeout(executeNextCommand, MOVEMENT_COOLDOWN_TIME_MS);
       }
     }
 
-    function update() {
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            moveTo(state.player.x, state.player.y - TILE_SIZE);
-            state.player.body.velocity.y = TILE_SIZE;
-        } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            // moveTo(state.player.x, state.player.y + TILE_SIZE);
-            // state.player.body.velocity.y = -TILE_SIZE;
-            movePlayer(0, 1)
-        }
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            moveTo(state.player.x - TILE_SIZE, state.player.y);
-        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            moveTo(state.player.x + TILE_SIZE, state.player.y);
-        }
-
-        // game.physics.arcade.collide(state.player, state.layer);
-    }
-
-    function render() {
-
-    }
-
-    window.vasen = function() { movePlayer(-1, 0); }
-    window.oikea = function() { movePlayer(1, 0); }
-    window.ylos = function() { movePlayer(0, -1); }
-    window.alas = function() { movePlayer(0, 1); }
-
-    window.execute = function() {
-      const text = document.getElementById("code").value;
-
-      if (text.length === 0) {
-        return;
-      }
-
-      const commands = text.split('\n');
-      console.log('commands', commands);
-
-      let currentCommandIndex = 0;
-
-      function executeNextCommand() {
-        const command = commands[currentCommandIndex];
-        console.log('executing command', command);
-        eval(command);
-
-        currentCommandIndex++;
-
-        if (currentCommandIndex < commands.length) {
-          setTimeout(executeNextCommand, 1000);
-        }
-      }
-
-      executeNextCommand();
-
-      // eval(document.getElementById("code").value);
-    }
+    executeNextCommand();
+  }
 }
--- src/main/map.js
+++ src/main/map.js
@@ -1,37 +1,11 @@
 "use strict";
 window.Phaser = require('phaser-ce/build/custom/phaser-split');
 
-module.exports.createMap = function(game, settings, tiles) {
-    const tileMap = game.add.tilemap();
-    tileMap.addTilesetImage(tiles.grass, undefined, settings.tileSize, settings.tileSize);
-    const layer = tileMap.create("layer", settings.tileCount, settings.tileCount, settings.tileSize, settings.tileSize);
-    for (let i = 0; i < settings.tileCount; i++) {
-        for (let j = 0; j < settings.tileCount; j++) {
-            tileMap.putTile(0, i, j, layer);
-        }
-    }
-    return tileMap;
-}
-
-module.exports.createLevel1 = function(game, settings, tiles) {
-  const tileMap = game.add.tilemap();
-  console.log('tiles', tiles)
-  tileMap.addTilesetImage(tiles);
-  tileMap.setCollisionBetween(1, 5); 
-  tileMap.setCollision(1);
-
-  const layer = tileMap.create("layerGround", settings.tileCount, settings.tileCount, settings.tileSize, settings.tileSize);
-  layer.resizeWorld();
-
-  // const collisionLayer = map.tileMap('Collision')
-  // collisionLayer.visible = false;
-  // tileMap.setCollisionByExclusion([], true, this.collisionLayer);
-  // collisionLayer.resizeWorld();
-
-  // 0 = grass (33,107)
-  // 1 = kanto
-  // 2 = porkkana
-  const mapData = [
+// 0 = grass (33,107)
+// 1 = kanto
+// 2 = porkkana
+const MAPS = {
+  level1: [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 2, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -42,21 +16,20 @@ module.exports.createLevel1 = function(game, settings, tiles) {
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-  ];
+  ]
+};
+
+module.exports.loadLevel = function(game, settings, tiles, levelNumber) {
+  const tileMap = game.add.tilemap();
+  tileMap.addTilesetImage(tiles);
+  const layer = tileMap.create("layerGround", settings.tileCount, settings.tileCount, settings.tileSize, settings.tileSize);
+  layer.resizeWorld();
+
+  const mapData = MAPS['level' + levelNumber];
 
   for (let y = 0; y < settings.tileCount; ++y) {
     for (let x = 0; x < settings.tileCount; ++x) {
-      if (mapData[y][x] === 0) {
-        tileMap.putTile(0, x, y, layer);
-      }
-
-      if (mapData[y][x] === 1) {
-        tileMap.putTile(1, x, y, layer);
-      }
-
-      if (mapData[y][x] === 2) {
-        tileMap.putTile(2, x, y, layer);
-      }
+      tileMap.putTile(mapData[y][x], x, y, layer);
     }
   }
   return {
